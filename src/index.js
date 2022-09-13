9ba3f9d8f78327ff9be8a724928f13d50a58f756
ANIMEFIGHTER <56396779+ANIMEFIGHTER@users.noreply.github.com>
2022-09-13 18:22:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,37 @@ import {Provider} from "react-redux";
 const defaultState = {
     tasks: [],
 }
+const ADD_TASK = "ADD_TASK"
+const DELETE_ALL = "DELETE_ALL"
+const DELETE_TASK = "DELETE_TASK"
+const CHECK_TASK = "CHECK_TASK"
+
+
 
 export const taskReducer = (state = defaultState, action) => {
     switch (action.type) {
-        case "ADD_TASK":
+        case ADD_TASK:
             console.log("Xuita")
             return {...state, tasks: [...state.tasks, action.payload] }
+        case DELETE_ALL:
+            return {...state, tasks: []}
+        case DELETE_TASK:
+            return {...state, tasks: state.tasks.filter(item => item.id !== action.payload)}
+        case CHECK_TASK:
+            return {...state, tasks: state.tasks.map((item) => {
+                    if (item.id === action.payload) {
+                        item.checked = !item.checked;
+                    }
+                    return item;
+                })}
         default:
             return state
     }
 }
+export const addTaskAction = (payload) => ({type: ADD_TASK, payload})
+export const deleteTaskAction = (payload) => ({type: DELETE_TASK, payload})
+export const deleteAllAction = () =>({type: DELETE_ALL})
+export const checkTask = (payload) => ({type: CHECK_TASK, payload})
 
 const store = createStore(taskReducer);
 
